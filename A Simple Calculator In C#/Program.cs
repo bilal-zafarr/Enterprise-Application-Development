358973d0ae08f3c9d76449d7a82e5679fcd4f93e
Bilal Zafar <bilalzaafar@gmail.com>
2020-10-19 02:00:43 +0500

    Add files via upload

---
+++ A Simple Calculator In C#/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EADAssi1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            string inp1 = Console.ReadLine();
+
+            Console.Write("Enter second number: ");
+            string inp2 = Console.ReadLine();
+
+            // initializing the numbers with 0 so that we can use then in try block
+
+            double num1=0, num2=0;
+
+            try
+            {
+                num1 = double.Parse(inp1);
+                num2 = double.Parse(inp2);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                System.Environment.Exit(0);
+            }
+
+            Console.Write("Enter operator (+, -, *, /, %): ");
+            char op = (char)Console.Read();
+
+            switch (op)
+            {
+                case '+':
+                    Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+                    break;
+                case '-':
+                    Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2);
+                    break;
+                case '*':
+                    Console.WriteLine("{0} * {1} = {2}", num1, num2, num1 * num2);
+                    break;
+                case '/':
+                    Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
+                    break;
+                case '%':
+                    Console.WriteLine("{0} % {1} = {2}", num1, num2, num1 % num2);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Operator");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ A class in C# that keeps counts of all its objects/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Xml.Schema;
+
+namespace EADAssi1
+{
+    class Program
+    {
+        public class test
+        {
+            private static int count = 0;
+
+            public static int getCount()
+            {
+                return count;
+            }
+            public test()
+            {
+                count++;
+            }
+            ~test()
+            {
+                count--;
+            }
+        }
+        static void Main(string[] args)
+        {
+            test obj = new test();
+            test obj2 = new test();
+            test obj3 = new test();
+            Console.WriteLine(Program.test.getCount());
+        }
+    }
+}
